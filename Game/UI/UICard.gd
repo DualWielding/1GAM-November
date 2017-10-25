946241af26c2777979788beedae4f05b9f197d2d
Maël ARNAUD <mael.arnaud@gmail.com>
2017-10-25 15:58:44 +0200

    Added right click mechanics on cards to inspect them

--- Game/UI/UICard.gd
+++ Game/UI/UICard.gd
@@ -8,10 +8,13 @@ var text = "Test"
 var img = preload("res://Sprites/Floor.png")
 var important = false
 
+var _hovered = false
+var _up = false
+
 var selectable = false setget set_selectable, is_selectable
 var selected = false setget set_selected, is_selected
 
-onready var aura = get_node("Aura")
+onready var aura = get_node("Wrapper/Aura")
 
 onready var ap = get_node("AnimationPlayer")
 
@@ -27,22 +30,34 @@ func init_from_dic(dic):
 	if dic.has("important") and dic.important:
 		important = true
 		add_to_group("ui_important_card")
-		get_node("VirginCard").set_modulate(Color("#9013d0"))
+		get_node("Wrapper/VirginCard").set_modulate(Color("#9013d0"))
 	else:
 		add_to_group("ui_normal_card")
 	add_to_group("ui_card")
 
 func _ready():
-	get_node("VirginCard/Name").set_text(name)
-	get_node("VirginCard/Picture").set_texture(img)
-	get_node("VirginCard/Text").set_text(text)
+	get_node("Wrapper/VirginCard/Name").set_text(name)
+	get_node("Wrapper/VirginCard/Picture").set_texture(img)
+	get_node("Wrapper/VirginCard/Text").set_text(text)
+	
+	set_process_input(true)
+
+func _input(event):
+	if _hovered\
+	and event.type == InputEvent.MOUSE_BUTTON\
+	and event.button_index == BUTTON_RIGHT\
+	and event.pressed:
+		if _up:
+			lower()
+		else:
+			bring_up()
 
 ##### SELECTION #####
 
 # This is only for discard purposes ATM
 
 func set_selectable(boolean):
-	get_node("VirginCard").set_pressed(false)
+	get_node("Wrapper/VirginCard").set_pressed(false)
 	selectable = boolean
 
 func is_selectable():
@@ -64,14 +79,26 @@ func is_selected():
 ##### ANIMATIONS #####
 
 func bring_up():
-	ap.play("bring_up")
+	if !selectable:
+		ap.play("bring_up")
+		_up = true
+		_hovered = false
 
 func lower():
-	ap.play_backwards("lower")
+	if !selectable:
+		ap.play_backwards("bring_up")
+		_up = false
+		_hovered = false
 
 func _on_VirginCard_toggled( pressed ):
 	if Player.ui.cards_to_discard_number == 0 and pressed:
-		get_node("VirginCard").set_pressed(false)
+		get_node("Wrapper/VirginCard").set_pressed(false)
 		return
 	if is_selectable():
 		set_selected(pressed)
+
+func _on_VirginCard_mouse_enter():
+	_hovered = true
+
+func _on_VirginCard_mouse_exit():
+	_hovered = false
