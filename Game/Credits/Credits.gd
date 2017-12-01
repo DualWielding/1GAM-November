40f822ee9e26f48983dd1d012282251f0acef215
Maël ARNAUD <mael.arnaud@gmail.com>
2017-12-01 17:24:32 +0100

    Finished da game

--- Game/Credits/Credits.gd
+++ Game/Credits/Credits.gd
@@ -5,8 +5,8 @@ onready var persons = get_node("Persons")
 func _ready():
 	get_node("MainMenu").set_text(tr("MAIN MENU"))
 	get_node("MailingList").set_bbcode(str("[url=http://eepurl.com/c6UwPD]", tr("MAILING LIST"), "[/url]"))
-	get_node("MailingList").append_bbcode(str("\n[url=www.dual-wielding.com]", tr("WEBSITE"), "[/url]"))
-	get_node("MailingList").append_bbcode(str("\n[url=https://goo.gl/forms/oaEj0U8nSDlOYB2c2]", tr("HELP US"), "[/url]"))
+	get_node("MailingList").append_bbcode(str("\n\n[url=www.dual-wielding.com]", tr("WEBSITE"), "[/url]"))
+	get_node("MailingList").append_bbcode(str("\n\n[url=https://goo.gl/forms/oaEj0U8nSDlOYB2c2]", tr("HELP US"), "[/url]"))
 	for child in get_node("Container").get_children():
 		child.connect("button_pressed", self, "show_list")
 	get_node("ColorFrame/AnimationPlayer").play("fade")
